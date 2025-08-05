3f1b2495c90eda33fa260c645675bf4aad63cfd1
Stefan Blagojevic <blagojevics710@gmail.com>
2025-08-05 20:27:08 +0200

    made some proggsss

--- src/Leftside/Leftside.jsx
+++ src/Leftside/Leftside.jsx
@@ -50,7 +50,7 @@ export default function Leftside() {
           </span>
         </div>
         <div className="total-container">
-          <h3>Total:</h3>
+          <h3>Total:</h3>fdsfsdf
 
           <span>Calories: {ingredientsList.calories}g</span>
           <span>Protein: {ingredientsList.protein} g</span>
@@ -58,6 +58,55 @@ export default function Leftside() {
           <span>Fat: {ingredientsList.fat} g</span>
           <span>Fiber: {ingredientsList.fiber} g</span>
           <span>Sugar: {ingredientsList.sugar} g</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+export default function Rightside() {
+  const [showNutrValue, setShowNutrValue] = useState({});
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleFoodDetails = (foodClicked) => {
+    setShowNutrValue(foodClicked);
+  };
+
+  const filteredItems = foods.filter((food) =>
+    food.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+  return (
+    <>
+      <div className="container">
+        <h2>NutriTracker</h2>
+        <input
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          type="search"
+          placeholder="Search..."
+        />
+        <ul>
+          {searchInput === "" ? (
+            <p>Start typing...</p>
+          ) : filteredItems.length === 0 ? (
+            <p>No result fount for {searchInput}</p>
+          ) : (
+            filteredItems.map((food) => (
+              <p onClick={() => handleFoodDetails(food)} key={food.id}>
+                {food.name}
+              </p>
+            ))
+          )}
+        </ul>
+        <div className="nutrients-container">
+          <h3>Nutrients for: {showNutrValue.name} </h3>
+          <span>Calories: {showNutrValue.calories} g</span>
+          <span>Protein: {showNutrValue.protein} g</span>
+          <span>Carbs: {showNutrValue.carbs} g</span>
+          <span>Fat: {showNutrValue.fat} g</span>
+          <span>Fiber: {showNutrValue.fiber} g</span>
+          <span>Sugar: {showNutrValue.sugar} g</span>
         </div>
       </div>
     </>
